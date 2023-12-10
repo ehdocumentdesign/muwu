af0d9d986d3df2a48484fe0a3ae8cf35501edd69
Eli Harrison <admin@eliharrison.com>
2023-12-10 00:07:24 -0800

    Fixed error when outline is empty.

--- lib/muwu/viewer/viewer.rb
+++ lib/muwu/viewer/viewer.rb
@@ -6,19 +6,27 @@ module Muwu
 
 
     def initialize(project)
-      document_home = project.manifest.find_document_html_by_index(0).filename
-      document_home_path = File.join(project.path_compiled, document_home)
-      if File.exists?(document_home_path)
+      document_home = project.manifest.find_document_html_by_index(0)
+      if document_home
+        document_home_filepath = File.join(project.path_compiled, document_home.filename)
+        initialize_viewer(document_home_filepath)
+      else
+        puts "No documents in outline to view."
+      end
+    end
+
+
+    def initialize_viewer(document_home_filepath)
+      if File.exist?(document_home_filepath)
         begin
-          system "lynx #{document_home_path}", exception: true
+          system "lynx #{document_home_filepath}", exception: true
         rescue Errno::ENOENT
           raise ProjectExceptionHandler::Fatal.new(ProjectException::LynxNotAvailable.new)
         end
       else
-        puts "Compiled document not found: #{document_home_path}"
+        puts "Compiled document not found: #{document_home_filepath}"
       end
     end
-
 
 
   end
